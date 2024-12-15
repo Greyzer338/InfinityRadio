b43cac5cbc4b8a7f6c74277b50e8d0137b100409
Greyzer <chaosphantom52@gmail.com>
2024-12-15 13:17:48 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿using Exiled.API.Interfaces;
+namespace InfinityRadio
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+namespace InfinityRadio
+{
+    public sealed class Plugin : Plugin<Config>
+    {
+        public override string Author => "Greyzer";
+        public override string Name => "InfinityRadio";
+        public override string Prefix => Name;
+        public override void OnEnabled() { Exiled.Events.Handlers.Player.UsingRadioBattery += OnUsingRadioBatteryEventArgs; }
+        public override void OnDisabled() { Exiled.Events.Handlers.Player.UsingRadioBattery -= OnUsingRadioBatteryEventArgs; }
+        private void OnUsingRadioBatteryEventArgs(UsingRadioBatteryEventArgs ev) => ev.Drain = 0f;
+    }
+}
